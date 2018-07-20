0c15b6ee00de0c86b0b6d3d4a58ec4bd36e8b0fe
katie stout <katiestout@katies-MBP.home>
2018-07-20 17:27:22 -0400

    third commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+//PORT = 8080;
+
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT);
